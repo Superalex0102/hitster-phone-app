3eb12c8e6ba9f4648c2aca7adc6657488cd15eab
Molnár Ferenc <nfecoka4@gmail.com>
2026-04-16 09:16:24 +0200

    Initial commit

---
+++ composeApp/src/androidUnitTest/kotlin/com/rdisoftware/chronobeat/ComposeAppAndroidUnitTest.kt
@@ -0,0 +1,12 @@
+package com.rdisoftware.chronobeat
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppAndroidUnitTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
