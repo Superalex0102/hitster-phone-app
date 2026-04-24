bcadfc0a6b5ffbb41030e1a7f76d4292c2299e1a
Molnár Ferenc <nfecoka4@gmail.com>
2026-04-24 13:44:43 +0200

    feat(playlist): implemented playlist getting from github pages
    # Conflicts:
    #	shared/src/commonMain/kotlin/com/rdisoftware/chronobeat/data/repositories/MusicRepositoryImpl.kt

--- shared/src/commonMain/kotlin/com/rdisoftware/chronobeat/data/repositories/MusicRepositoryImpl.kt
+++ shared/src/commonMain/kotlin/com/rdisoftware/chronobeat/data/repositories/MusicRepositoryImpl.kt
@@ -1,19 +1,24 @@
 package com.rdisoftware.chronobeat.data.repositories
 
+import com.rdisoftware.chronobeat.data.mappers.toDomain
+import com.rdisoftware.chronobeat.data.remote.api.ChronoBeatApi
+import com.rdisoftware.chronobeat.domain.models.Playlist
 import com.rdisoftware.chronobeat.domain.models.Track
 import com.rdisoftware.chronobeat.domain.repositories.MusicRepository
 import com.rdisoftware.chronobeat.domain.repositories.TokenRepository
 
 class MusicRepositoryImpl(
-    // TODO: private val apiService: MusicApiService,
+    private val chronoBeatApi: ChronoBeatApi,
     private val tokenRepository: TokenRepository
 ) : MusicRepository {
     override suspend fun getUserPlaylists() {
         TODO("Not yet implemented")
     }
 
-    override suspend fun getChronobeatPlaylists() {
-        TODO("Not yet implemented")
+    override suspend fun getChronobeatPlaylists() : List<Playlist>{
+        val responeDto = chronoBeatApi.fetchChronoBeatPlaylists()
+        return responeDto.toDomain()
+
     }
 
     override suspend fun getTrackInfo(trackId: String): Track {
--- shared/src/commonMain/kotlin/com/rdisoftware/chronobeat/domain/repositories/MusicRepository.kt
+++ shared/src/commonMain/kotlin/com/rdisoftware/chronobeat/domain/repositories/MusicRepository.kt
@@ -1,10 +1,11 @@
 package com.rdisoftware.chronobeat.domain.repositories
 
+import com.rdisoftware.chronobeat.domain.models.Playlist
 import com.rdisoftware.chronobeat.domain.models.Track
 
 interface MusicRepository {
     suspend fun getUserPlaylists()
-    suspend fun getChronobeatPlaylists()
+    suspend fun getChronobeatPlaylists(): List<Playlist>
     suspend fun getTrackInfo(trackId: String): Track
     suspend fun playMusic()
     suspend fun pauseMusic()
