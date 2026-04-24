5d71ac8072e2c9566f378cf0f6905c3b9e6c9bf0
Molnár Ferenc <nfecoka4@gmail.com>
2026-04-24 13:37:34 +0200

    fix(MusicMapper): removed class to use outside the class scope

--- shared/src/commonMain/kotlin/com/rdisoftware/chronobeat/data/mappers/MusicMapper.kt
+++ shared/src/commonMain/kotlin/com/rdisoftware/chronobeat/data/mappers/MusicMapper.kt
@@ -5,25 +5,23 @@ import com.rdisoftware.chronobeat.data.remote.dto.MusicCardDto
 import com.rdisoftware.chronobeat.domain.models.Track
 import com.rdisoftware.chronobeat.domain.models.Playlist
 
-class MusicMapper {
-    fun MusicCardDto.toDomain(): Track {
-        return Track(
-            id = this.id,
-            title = this.name,
-            mainArtist = this.artists[0].name,
-            featArtists = this.artists.drop(1).map { it.name },
-            releaseYear = this.albumDto.releaseDate.split("-")[0].toInt(),
-            isPlayable = this.isPlayable
-        )
-    }
+fun MusicCardDto.toDomain(): Track {
+    return Track(
+        id = this.id,
+        title = this.name,
+        mainArtist = this.artists[0].name,
+        featArtists = this.artists.drop(1).map { it.name },
+        releaseYear = this.albumDto.releaseDate.split("-")[0].toInt(),
+        isPlayable = this.isPlayable
+    )
+}
 
-    fun ChronoBeatPlaylistResponseDto.toDomain(): List<Playlist> {
-        return this.playlists.map { playlist ->
-            Playlist(
-                id = playlist.id,
-                name = playlist.name,
-                trackIds = playlist.trackIds
-            )
-        }
+fun ChronoBeatPlaylistResponseDto.toDomain(): List<Playlist> {
+    return this.playlists.map { playlist ->
+        Playlist(
+            id = playlist.id,
+            name = playlist.name,
+            trackIds = playlist.trackIds
+        )
     }
 }
