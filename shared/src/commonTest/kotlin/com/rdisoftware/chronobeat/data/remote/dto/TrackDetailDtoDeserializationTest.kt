2e379d49e0e77f719ede432b8b2000c7c5fa7264
Molnár Ferenc <nfecoka4@gmail.com>
2026-05-08 10:03:45 +0200

    feat(token): fix tokenmanager

--- shared/src/commonTest/kotlin/com/rdisoftware/chronobeat/data/remote/dto/TrackDetailDtoDeserializationTest.kt
+++ shared/src/commonTest/kotlin/com/rdisoftware/chronobeat/data/remote/dto/TrackDetailDtoDeserializationTest.kt
@@ -8,7 +8,7 @@ import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
 
-class TrackDetailDtoDeserializationTest {
+class  TrackDetailDtoDeserializationTest {
 
     private val json = Json { ignoreUnknownKeys = true }
 
