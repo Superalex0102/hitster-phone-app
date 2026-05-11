06ecdd0906b8a891efb9b7c97fb6e6529470e273
rdipatrik <rdipatrik@gmail.com>
2026-05-11 10:00:39 +0200

    Implemented unit tests for TeamSelectionViewModel.kt

---
+++ shared/src/commonTest/kotlin/com/rdisoftware/chronobeat/testing/TeamSelectionViewModelTest.kt
@@ -0,0 +1,206 @@
+package com.rdisoftware.chronobeat.testing
+
+import com.rdisoftware.chronobeat.domain.enums.TeamColor
+import com.rdisoftware.chronobeat.presentation.viewmodels.TeamSelectionViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+import kotlin.uuid.ExperimentalUuidApi
+
+@OptIn(
+    ExperimentalCoroutinesApi::class,
+    ExperimentalUuidApi::class
+)
+class TeamSelectionViewModelTest {
+    lateinit var viewModel: TeamSelectionViewModel
+    @BeforeTest
+    fun setup() {
+        Dispatchers.setMain(UnconfinedTestDispatcher())
+        viewModel = TeamSelectionViewModel()
+    }
+
+    @AfterTest
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    private fun TestScope.addTeam(name: String) {
+        viewModel.onNameChanged(newName = name)
+        viewModel.addTeam()
+        advanceUntilIdle()
+    }
+
+    @Test
+    fun `addTeam should add a new team when input is valid`() = runTest {
+        addTeam("Team 1")
+
+        assertEquals(
+            expected = 1,
+            actual = viewModel.state.value.teams.size,
+            message = "addTeam should add a new team to the list when input is valid"
+        )
+        assertEquals(
+            expected = "Team 1",
+            actual = viewModel.state.value.teams.first().name,
+            message = "addTeam should use the provided input name"
+        )
+    }
+
+    @Test
+    fun `addTeam should not add team when max limit is reached`() = runTest {
+        addTeam("Team 1")
+        addTeam("Team 2")
+        addTeam("Team 3")
+        addTeam("Team 4")
+        addTeam("Team 5")
+
+        assertEquals(
+            expected = 4,
+            actual = viewModel.state.value.teams.size,
+            message = "addTeam should not add more teams when max limit is reached"
+        )
+    }
+
+    @Test
+    fun `addTeam should not add team when input contains only whitespace`() = runTest {
+        addTeam("   ")
+
+        assertTrue(
+            actual = viewModel.state.value.teams.isEmpty(),
+            message = "addTeam doesn't take white space strings as an input"
+        )
+    }
+
+    @Test
+    fun `addTeam should trim out the white spaces`() = runTest {
+        addTeam("Team 1")
+        addTeam("   Team 2   ")
+        val secondTeam = viewModel.state.value.teams[1].name
+
+
+        assertEquals(
+            expected = "Team 2",
+            actual = secondTeam,
+            message = "addTeam should trim input name before adding team"
+        )
+    }
+
+    @Test
+    fun `deleteTeam should delete existing team`() = runTest {
+        addTeam("Team 1")
+        advanceUntilIdle()
+        viewModel.deleteTeam(teamId =  viewModel.state.value.teams.first().id)
+
+        assertTrue(
+            actual = viewModel.state.value.teams.isEmpty(),
+            message = "deleteTeam should remove team from list when ID exists"
+        )
+    }
+
+    @Test
+    fun `onNameChanged should ignore values when exceeding max length`() {
+        viewModel.onNameChanged("Valid")
+        viewModel.onNameChanged("Longer than 16 characters")
+
+        assertEquals(
+            expected = "Valid",
+            actual = viewModel.state.value.inputName,
+            message = "onNameChanged should ignore input exceeding max length"
+        )
+    }
+
+    @Test
+    fun `startEdit should set editing state correctly`() = runTest {
+        addTeam("Team 1")
+        advanceUntilIdle()
+        val team = viewModel.state.value.teams.first()
+        viewModel.startEdit(team)
+        val state = viewModel.state.value
+
+        assertEquals(
+            expected = team,
+            actual = state.editingTeam,
+            message = "startEdit should set the editingTeam"
+        )
+
+        assertEquals(
+            expected = team.name,
+            actual = state.inputName,
+            message = "startEdit should copy team name to inputName"
+        )
+
+        assertTrue(
+            actual = state.isEditing,
+            message = "startEdit should set isEditing to true"
+        )
+    }
+
+    @Test
+    fun `confirmEdit should update team name when input is valid`() = runTest {
+        addTeam("Team 1")
+        advanceUntilIdle()
+        val team = viewModel.state.value.teams.first()
+        viewModel.startEdit(team)
+        viewModel.onNameChanged("Team 5")
+        viewModel.confirmEdit()
+
+        assertEquals(expected = "Team 5",
+            actual = viewModel.state.value.teams.first().name,
+            message = "confirmEdit should update team name when input is within max length"
+        )
+    }
+
+    @Test
+    fun `confirmEdit should not update team when input is invalid`() = runTest {
+        addTeam("Team 1")
+        advanceUntilIdle()
+        val team = viewModel.state.value.teams.first()
+        viewModel.startEdit(team)
+        viewModel.onNameChanged("Longer than 16 characters")
+        viewModel.confirmEdit()
+
+        assertEquals(
+            expected = team.name,
+            actual = viewModel.state.value.teams.first().name,
+            message = "confirmEdit should not update team name when input exceeds max length"
+        )
+    }
+
+    @Test
+    fun `updateTeam should replace team when ID matches`() = runTest {
+        addTeam("Team 1")
+        val originalTeam = viewModel.state.value.teams.first()
+
+        val updatedTeam = originalTeam.copy(name = "Updated team")
+        viewModel.updateTeam(updatedTeam)
+
+        assertEquals(
+            expected ="Updated team",
+            actual = viewModel.state.value.teams.first().name,
+            message = "updateTeam should replace the team's name with the updated value"
+        )
+    }
+
+    @Test
+    fun `onColorChanged updates selectedColor`() {
+        val newColor = TeamColor.entries[3]
+
+        viewModel.onColorChanged(newColor)
+
+        assertEquals(
+            expected = newColor,
+             actual = viewModel.state.value.selectedColor,
+            message = "onColorChanged should update selectedColor"
+        )
+    }
+}
