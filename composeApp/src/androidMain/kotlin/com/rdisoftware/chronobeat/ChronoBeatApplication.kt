f78732e86c6c8133d1bf83fd608f7befa6cace7c
Molnár Ferenc <nfecoka4@gmail.com>
2026-05-14 16:25:30 +0200

    feat(android): initialize Koin application and inject Spotify controller

--- composeApp/src/androidMain/kotlin/com/rdisoftware/chronobeat/ChronoBeatApplication.kt
+++ composeApp/src/androidMain/kotlin/com/rdisoftware/chronobeat/ChronoBeatApplication.kt
@@ -1,4 +1,22 @@
 package com.rdisoftware.chronobeat
 
-class Application {
+import android.app.Application
+import android.content.Context
+import com.rdisoftware.chronobeat.di.initKoin
+import org.koin.dsl.module
+
+class ChronoBeatApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        val androidAppModule = module {
+            single<Context> { this@ChronoBeatApplication }
+            single<Application> { this@ChronoBeatApplication }
+        }
+
+        initKoin {
+            printLogger()
+            modules(androidAppModule)
+        }
+    }
 }
--- composeApp/src/androidMain/kotlin/com/rdisoftware/chronobeat/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/rdisoftware/chronobeat/MainActivity.kt
@@ -6,7 +6,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.rdisoftware.chronobeat.data.auth.TokenManager
+import com.rdisoftware.chronobeat.data.remote.api.ChronoBeatApi
+import com.rdisoftware.chronobeat.data.repositories.MusicRepositoryImpl
 import com.rdisoftware.chronobeat.domain.player.AndroidSpotifyController
+import com.rdisoftware.chronobeat.domain.player.SpotifyPlayerController
+import com.rdisoftware.chronobeat.presentation.screens.SpotifyPlayerScreen
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import org.koin.core.context.loadKoinModules
+import org.koin.dsl.module
 
 class MainActivity : ComponentActivity() {
     lateinit var spotifyController: AndroidSpotifyController
@@ -16,6 +26,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         spotifyController = AndroidSpotifyController(this, TokenManager)
 
+        loadKoinModules(module {
+            single<SpotifyPlayerController> { spotifyController }
+        })
+
         setContent {
             // TODO (Architecture - Spotify & MusicRepository):
             // In the future, when screens need the music player, DO NOT pass it as a parameter to App()!
@@ -23,6 +37,7 @@ class MainActivity : ComponentActivity() {
             // The DI module will call 'RepositoryFactory.createMusicRepository(spotifyController)'
             // and inject the ready-to-use repository directly into the target Screen's ViewModel.
             App()
+            //SpotifyPlayerScreen()
         }
     }
 
