c8a86864bbfd8f14bb9518ff351dcb4cb54749c0
Molnár Ferenc <nfecoka4@gmail.com>
2026-05-14 12:18:23 +0200

    feat(di): Added dependencies to project

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -47,9 +47,12 @@ kotlin {
             implementation(libs.ktor.client.logging)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.koin.core)
+            implementation(libs.koin.compose)
         }
         androidMain.dependencies {
             implementation(libs.ktor.client.okhttp)
+            implementation(libs.koin.android)
         }
         iosMain.dependencies {
             implementation(libs.ktor.client.darwin)
