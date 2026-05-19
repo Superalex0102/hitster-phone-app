73927fa43ccd5233a8d0cccd6a29366effd430b6
Molnár Ferenc <nfecoka4@gmail.com>
2026-05-19 11:26:38 +0200

    fix: added github actions secret

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -91,7 +91,7 @@ buildConfig {
     val spotifyBaseUrl = localProps.getProperty("SPOTIFY_BASE_URL") ?: "https://api.spotify.com/v1/"
     buildConfigField("String", "SPOTIFY_BASE_URL", "\"$spotifyBaseUrl\"")
 
-    val chronobeatBaseUrl = localProps.getProperty("CHRONOBEAT_BASE_URL") ?: ""
+    val chronobeatBaseUrl = System.getenv("CHRONOBEAT_BASE_URL") ?: localProps.getProperty("CHRONOBEAT_BASE_URL") ?: ""
     buildConfigField("String", "CHRONOBEAT_BASE_URL", "\"$chronobeatBaseUrl\"")
 
     val spotifyClientId = localProps.getProperty("SPOTIFY_CLIENT_ID") ?: ""
