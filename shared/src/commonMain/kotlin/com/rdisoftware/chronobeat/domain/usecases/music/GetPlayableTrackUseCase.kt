20bf0f57230dca281b9b56b82db4fc037b80a204
Molnár Ferenc <nfecoka4@gmail.com>
2026-06-03 12:24:13 +0200

    fix: added mutex to avoid race condition

--- shared/src/commonMain/kotlin/com/rdisoftware/chronobeat/domain/usecases/music/GetPlayableTrackUseCase.kt
+++ shared/src/commonMain/kotlin/com/rdisoftware/chronobeat/domain/usecases/music/GetPlayableTrackUseCase.kt
@@ -2,16 +2,23 @@ package com.rdisoftware.chronobeat.domain.usecases.music
 
 import com.rdisoftware.chronobeat.domain.models.Track
 import com.rdisoftware.chronobeat.domain.repositories.MusicRepository
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
 class GetPlayableTrackUseCase(private val musicRepository: MusicRepository) {
+    private val mutex = Mutex()
     suspend operator fun invoke(trackIdPool: MutableList<String>): Track? {
-        while (trackIdPool.isNotEmpty()) {
-            val nextId = trackIdPool.removeAt(0)
+        while (true) {
+            val nextId = mutex.withLock {
+                if (trackIdPool.isNotEmpty()) trackIdPool.removeAt(0) else null
+            }
+
+            if (nextId == null) return null
+
             runCatching {
                 val track = musicRepository.getTrackInfo(nextId)
                 if (track.isPlayable) return track
             }
         }
-        return null
     }
 }
