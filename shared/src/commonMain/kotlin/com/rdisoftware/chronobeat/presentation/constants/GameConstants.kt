8c0e4ab52910f63b9619cfc2dd515713c7dd3d22
Molnár Ferenc <35114741+MolnarFerenc123@users.noreply.github.com>
2026-06-10 14:17:14 +0200

    feat: change card to guess to 5 to show (#47)

--- shared/src/commonMain/kotlin/com/rdisoftware/chronobeat/presentation/constants/GameConstants.kt
+++ shared/src/commonMain/kotlin/com/rdisoftware/chronobeat/presentation/constants/GameConstants.kt
@@ -1,7 +1,7 @@
 package com.rdisoftware.chronobeat.presentation.constants
 
 object GameConstants {
-    const val CARDS_TO_WIN = 3
+    const val CARDS_TO_WIN = 5
     const val SAVED_GAME = "SAVED_GAME"
     const val SAVED_TEAMS = "SAVED_TEAMS"
 }
